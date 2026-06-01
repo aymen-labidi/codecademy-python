33d6d1a8286b35415a1700c5e89c04f2b3d79684
aymen-labidi <aymen.labidi.cs@outlook.com>
2026-06-01 09:17:22 -0500

    Create comments.py

---
+++ comments.py
@@ -0,0 +1,9 @@
+# This variable will be used to count the hnumber of times anyone tweets the word persnickety
+
+persnickety_count = 0 
+
+# This code will calculate the likelihood that it will rain tomorrow
+
+complicated_rain_cakculation_for_tomorrow () 
+
+# The first program I want to write is a program for a personal finance dashboard
