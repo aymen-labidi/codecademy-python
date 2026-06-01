5b74e26424edb17a496a8990b1dcacddbcb04aaf
aymen-labidi <aymen.labidi.cs@outlook.com>
2026-06-01 09:55:59 -0500

    Update exponents.py

--- Hello World/exponents.py
+++ Hello World/exponents.py
@@ -2,15 +2,15 @@
 
 # 6x6 quilt
 
-print ( 6**2 )
+print ( 6 ** 2 )
 
 # 7x7 quilt
 
-print ( 7**2 ) 
+print ( 7 ** 2 ) 
 
 # 8x8 quilt
 
-print ( 8**2 )
+print ( 8 ** 2 )
 
 # How many squares for 6 people to have 6 quilts each that are 6x6?
 
